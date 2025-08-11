ca7d7d44b49f2d95397e4dae534066c5604d7971
adrianograms <adrianograms@hotmail.com>
2025-08-11 10:55:17 -0300

    test push

--- extract_my_anime_list.py
+++ extract_my_anime_list.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv, set_key
 import os
 import json
 
-# Teste commit 2
+# Teste commit 3
 def get_token_myanimelist():
     # Load environment variables from .env file
     load_dotenv()
